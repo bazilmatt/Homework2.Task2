74187ed1ac4e3fed6e37e306224373a6c4687509
Vasiliy Matveev <bazilmat@yandex.ru>
2020-05-01 19:29:18 +0300

    New relize - добавлены исключения, соответствие заданию по выводу чисел квадрат целой части которых...

--- src/main/java/Homework2Task2.java
+++ src/main/java/Homework2Task2.java
@@ -1,4 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.invoke.WrongMethodTypeException;
 import java.math.*;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 /**
  *Составить программу, генерирующую N случайных чисел.
  * Для каждого числа k вычислить квадратный корень q.
@@ -11,27 +21,42 @@ import java.math.*;
 
 public class Homework2Task2 {
 
-    public static void main(String[] args) {
-        double[] randomArray = new double[10];
-        for (int i=0; i <randomArray.length ; i++) {
-            randomArray[i]=(Math.random()*100)-50;
-            System.out.println(randomArray[i]);
-        }
-        for (int i=0; i<=randomArray.length; i++){
-            try{
-                System.out.println(getSqrt(randomArray[i]));
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Получено отрицательное число");
+    public static void main(String[] args) throws IOException {
+
+            Scanner sc = new Scanner(System.in);
+            int index = sc.nextInt();
+            double[] randomArray = new double[index];
+            for (int i = 0; i < index; i++) {
+                randomArray[i] = (Math.random() * 100-40);
+                //System.out.println(randomArray[i]);
             }
-        }
+            for (double d:randomArray) {
+                try {
+                   // System.out.println("_____________________");
+                    if (Math.pow(getSqrt(d),2)==(int)d){
+                        System.out.println("Квадрат целой части квадратного корня равен числу = "+ (int)d);
+
+                    }
+
+
+                } catch (WrongMethodTypeException e) {
+                    System.out.println("Получено отрицательное число: "+d);
+                }
+            }
 
     }
     /** Возвращает квадратный корень числа.
-     * В случае передачи отрицательного значения - вернут IndexOutOfBoundsException */
-    static double getSqrt(double x) throws IndexOutOfBoundsException{
+     * В случае передачи отрицательного значения - вернут WrongMethodTypeException */
+    static int getSqrt(double x) throws WrongMethodTypeException {
         if (x<0){
-            throw new IndexOutOfBoundsException();
+            throw new WrongMethodTypeException();
+        } else {
+            int sqrtx = (int) Math.sqrt(x);
+          //  if (Math.pow(sqrtx,2)==(int)x){
+          //     return (int)x;
+            return sqrtx;
+            }
+
         }
-        return Math.sqrt(x);
     }
-}
+
